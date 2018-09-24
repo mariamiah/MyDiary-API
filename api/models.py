e4f860b9c5d6a2f9dfae7f197bb6507af2e2252b
= <=>
2018-09-24 19:20:52 +0300

    Modify models file

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
+from . import entries
+
+
 class User:
     """The User is the person who interacts with the application """
     def __init__(self, user_id, first_name, last_name, email, password):
@@ -7,18 +10,6 @@ class User:
         self.email = email
         self.password = password
         self.registered_users = dict()  # To keep track of the registered users
-
-    def register_user(self):
-        """Registers a User """
-        pass
-
-    def login_user(self):
-        """Logs in  a user """
-        pass
-
-    def get_user(self):
-        """Fetches registered users"""
-        pass
 
 
 class Diary:
@@ -30,22 +21,6 @@ class Diary:
         self.date_modified = date_modified
         self.registered_diaries = dict()
 
-    def add_entry(self):
-        """Adds a diary entry"""
-        pass
-
-    def edit_entry(self):
-        """Edits a diary entry"""
-        pass
-
-    def delete_entry(self):
-        """Deletes a diary entry"""
-        pass
-
-    def get_entry(self):
-        """Fetches a diary entry"""
-        pass
-
 
 class Entry:
     """This class represents all possible entries made by user"""
@@ -54,3 +29,15 @@ class Entry:
         self.date_added = date_added
         self.date_modified = date_modified
         self.entry_text = entry_text
+
+    def create_entry(self):
+        "creates an entry"
+        global entries
+        new_entry = {
+            "entry_id": self.entry_id,
+            "date_added": self.date_added,
+            "date_modified": self.date_modified,
+            "entry_text": self.entry_text
+        }
+        entries.append(new_entry)
+        return True
